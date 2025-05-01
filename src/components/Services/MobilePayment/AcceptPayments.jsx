f0d81b861803e946c0c57330e0bf13794028e8b6
Shrabon Tambuli <shrabonsa@gmail.com>
2025-05-01 23:57:19 +0600

    changes mobile pay page

--- src/components/Services/MobilePayment/AcceptPayments.jsx
+++ src/components/Services/MobilePayment/AcceptPayments.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const AcceptPayments = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto lg:px-10 p-4 md:pt-0 lg:pb-20">
-      <div className="md:flex md:flex-row-reverse items-center gap-8 lg:px-20">
+    <div className="max-w-7xl mx-auto px-2 lg:px-0 mb-24">
+      <div className="md:flex md:flex-row-reverse items-center gap-8">
         <div className="md:w-10/12">
-          <span className="inline-block w-60 py-1 bg-red-100 text-red-600 rounded-full font-semibold text-lg text-center tracking-widest">
+          <span className="inline-block w-60 py-1 bgs text2 rounded-full font-semibold text-lg text-center tracking-widest">
             Vendrax Solution
           </span>
-          <h1 className="text-3xl md:text-4xl lg:text-6xl text-black font-bold mt-10 w-11/12">
+          <h1 className="text-xl lg:text-5xl text2 font-bold mt-10">
             Accept Payments on Your Smartphones & Tablets
           </h1>
-          <p className="text-xl md:w-11/12 mt-10 md:mt-14 text-gray-500">
+          <p className="text-xl md:w-11/12 mt-10 text-gray-500">
             Experience the freedom and versatility of Vendrax mobile payment
             solutions. We introduce you to the payment solution that
             revolutionizes how you accept payments. With our innovative
@@ -21,50 +21,46 @@ const AcceptPayments = () => {
             and from any location.
           </p>
           <Link to="/">
-            <button className="flex items-center justify-center bg-[#228800] text-white font-semibold px-6 py-4 rounded-full hover:bg-[#00c6c0] transition-all w-64 text-lg relative uppercase mt-10">
+            <button className="flex items-center justify-center btn1 mt-10">
               Get Started
-              <img
-                className="absolute -right-3"
-                src="/images/red-btn.webp"
-                alt=""
-              />
             </button>
           </Link>
         </div>
         <div>
           <img
-            className="w-[850px] h-[654px] md:h-[400px] lg:h-[670px] mt-10 md:mt-0"
+            className="object-cover mt-10 md:mt-0"
             src="/images/mp-img1.webp"
             alt="img"
           />
         </div>
       </div>
-      <div className="md:flex  gap-8 lg:px-20 mt-20">
+      <div className="md:flex items-center gap-8 lg:px-20 mt-20">
         <div className="md:w-10/12">
-          <span className="inline-block w-60 py-1 bg-red-100 text-red-600 rounded-full font-semibold text-lg text-center tracking-widest">
+          <span className="inline-block w-60 py-1 bgs text2 rounded-full font-semibold text-lg text-center tracking-widest">
             Vendrax Solution
           </span>
-          <h1 className="text-3xl md:text-4xl lg:text-6xl text-black font-bold mt-10 w-11/12">
-          Other <br />
-          Perks
+          <h1 className="text-2xl lg:text-5xl text2 font-bold mt-10">
+            Other Perks
           </h1>
-          <p className="text-xl md:w-11/12 mt-10 md:mt-14 text-gray-500">
-          Mobile POS solutions are highly scalable and adaptable to your business growth – whether you operate a large retail chain or a small boutique – mobile POS systems accommodate your evolving needs, and expanding product lines. Moreover, many mobile POS solutions support customer loyalty programs, allowing you to incentivize repeat purchases, reward loyal customers, and build long-term relationships. These programs contribute to customer retention and increase customer lifetime value.
+          <p className="text-xl mt-10 text-gray-500">
+            Mobile POS solutions are highly scalable and adaptable to your
+            business growth – whether you operate a large retail chain or a
+            small boutique – mobile POS systems accommodate your evolving needs,
+            and expanding product lines. Moreover, many mobile POS solutions
+            support customer loyalty programs, allowing you to incentivize
+            repeat purchases, reward loyal customers, and build long-term
+            relationships. These programs contribute to customer retention and
+            increase customer lifetime value.
           </p>
           <Link to="/">
-            <button className="flex items-center justify-center bg-[#228800] text-white font-semibold px-6 py-4 rounded-full hover:bg-[#00c6c0] transition-all w-64 text-lg relative uppercase mt-10">
+            <button className="flex items-center justify-center btn1 mt-10">
               Get Started
-              <img
-                className="absolute -right-3"
-                src="/images/red-btn.webp"
-                alt=""
-              />
             </button>
           </Link>
         </div>
         <div>
           <img
-            className="w-[950px] h-[554px] md:h-[400px] lg:h-[570px] mt-10 md:mt-0"
+            className="object-cover mt-10 md:mt-0"
             src="/images/mp-img2.webp"
             alt="img"
           />
--- src/components/Services/MobilePayment/EasyPayments.jsx
+++ src/components/Services/MobilePayment/EasyPayments.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const EasyPayments = () => {
   return (
-    <div className="md:max-w-screen-2xl mx-auto lg:px-10 py-32 md:pt-20=">
+    <div className="max-w-7xl mx-auto px-2 lg:px-0 my-24">
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
-        <div className="space-y-10 p-4">
-          <span className="inline-block w-60 py-1 bg-red-100 text-red-600 rounded-full font-semibold text-lg text-center tracking-widest">
+        <div className="space-y-10">
+          <span className="inline-block w-60 py-1 bgs text2 rounded-full font-semibold text-lg text-center tracking-widest">
             Why Vendrax
           </span>
-          <h2 className="text-3xl md:text-4xl lg:text-6xl font-bold text-gray-900">
+          <h2 className="text-2xl lg:text-5xl font-bold text2">
             Easy Payments. Anywhere. Anytime.
           </h2>
           <p className="text-gray-600 text-xl">
@@ -20,18 +20,13 @@ const EasyPayments = () => {
             processing.
           </p>
           <Link to="/">
-            <button className="flex items-center justify-center bg-[#228800] text-white font-semibold px-6 py-4 rounded-full hover:bg-[#00c6c0] transition-all w-64 text-lg relative uppercase mt-10">
+            <button className="flex items-center justify-center btn1 mt-10">
               Get Started
-              <img
-                className="absolute -right-3"
-                src="/images/red-btn.webp"
-                alt=""
-              />
             </button>
           </Link>
         </div>
-        <div className="relative h-[960px] w-[390px] mx-auto lg:mx-0 md:w-[844px] md:h-[650px] lg:h-[708px] lg:w-[624px]">
-          <div className="bg-[#122e38] p-5 md:p-10 rounded-3xl space-y-8 h-full w-full absolute z-30 md:space-y-20 lg:space-y-10">
+        <div className="relative mx-auto h-[1070px] w-[370px] md:w-[828px] md:h-[700px] lg:h-[708px] lg:w-[624px]">
+          <div className="bg1 p-5 md:p-10 rounded-3xl space-y-8 h-full w-full absolute z-30 md:space-y-20 lg:space-y-10">
             <div className="flex items-center gap-6">
               <img
                 className="w-20 h-20"
@@ -39,7 +34,7 @@ const EasyPayments = () => {
                 alt="icon"
               />
               <div>
-                <h3 className="text-2xl text-[#17dcd6] font-medium mb-4">
+                <h3 className="text-2xl text-white font-medium mb-4">
                   Increase Your Revenue
                 </h3>
                 <p className="text-gray-300">
@@ -58,7 +53,7 @@ const EasyPayments = () => {
                 alt="icon"
               />
               <div>
-                <h3 className="text-2xl text-[#17dcd6] font-medium mb-4">
+                <h3 className="text-2xl text-white font-medium mb-4">
                 Enhanced Customer Experienced
                 </h3>
                 <p className="text-gray-300">
@@ -73,7 +68,7 @@ const EasyPayments = () => {
                 alt="icon"
               />
               <div>
-                <h3 className="text-2xl text-[#17dcd6] font-medium mb-4">
+                <h3 className="text-2xl text-white font-medium mb-4">
                 Reduce Overhead Costs
                 </h3>
                 <p className="text-gray-300">
@@ -82,14 +77,14 @@ const EasyPayments = () => {
               </div>
             </div>
           </div>
-          <div className="absolute -bottom-5 -ight-0 md:-bottom-5 md:-right-5 lg:-right-5 h-[460px] w-[300px] md:h-[460px] md:w-[560px] bg-[#ffbf41] z-20 rounded-3xl"></div>
+          <div className="absolute hidden lg:flex -bottom-5 -right-0 h-[460px] w-[560px] bg-[#ffbf41] z-20 rounded-3xl"></div>
           <img
-            className="absolute -top-7 -left-7"
+            className="absolute hidden lg:flex -top-7 -left-7"
             src="/images/bg2.webp"
             alt="icon"
           />
           <img
-            className="absolute z-10 -bottom-16 right-0 md:right-0 md:-bottom-28 lg:-bottom-32 lg:-right-7"
+            className="absolute hidden lg:flex z-10 -bottom-32 -right-0"
             src="/images/bg1.webp"
             alt="icon"
           />
@@ -97,7 +92,7 @@ const EasyPayments = () => {
       </div>
 
       <div
-        className=" mt-40 md:mt-60 h-[500px] mx-auto rounded-3xl"
+        className="mt-40 h-[500px] mx-auto rounded-3xl"
         style={{
           backgroundImage: "url(/images/pay-img.webp)",
           backgroundRepeat: "no-repeat",
